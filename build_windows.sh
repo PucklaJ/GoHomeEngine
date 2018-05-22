dd52993a1291aeb567a5ac2549f707079d195375
JohnnysCloud <jessica35@gmx.at>
2018-05-22 19:27:19 +0200

    Configured dependencies on windows

--- build_windows.sh
+++ build_windows.sh
@@ -21,8 +21,17 @@ then
 	cd $workingDir
 fi
 
+if [ ! -f $GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/bsreplacer/bsreplacer.exe ]
+then
+	echo Compiling backslash replacer ...
+	workingDir=$(pwd)
+	cd $GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/bsreplacer
+	$GOROOT/bin/go build
+	cd $workingDir
+fi
+
 PKG_CONFIG_PATH=${GOPATH}/src/github.com/PucklaMotzer09/gohomeengine/deps/pkg/windows
-$GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/fsreplacer/fsreplacer $PKG_CONFIG_PATH/assimp.pc '%GOPATH%' $GOPATH
+$GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/fsreplacer/fsreplacer $PKG_CONFIG_PATH/assimp.pc '%GOPATH%' $($GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/bsreplacer/bsreplacer.exe $GOPATH)
 
 wantInstall=0
 wantRun=0
@@ -31,7 +40,6 @@ wantRelease=0
 placedPath=""
 compileFlags=''
 projectDir=$(pwd)
-projectDir=${projectDir#${GOPATH}"/src/"}
 executableName=${projectDir#*/}
 for i in {1..20}
 do
@@ -46,8 +54,7 @@ do
 		wantInstall=1
 	elif [ "$arg" = "-run" ]
 	then
-		wantRun=0
-		echo -run does not work on windows
+		wantRun=1
 	elif [ "$arg" = "-help" ]
 	then
 		wantHelp=1
@@ -70,7 +77,7 @@ fi
 
 if [ $wantRelease -eq 1 ]
 then
-	compileFlags='-ldflags "-s"'
+	compileFlags='-ldflags -H=windowsgui'
 fi
 
 echo "Compiling $executableName ..."
@@ -78,7 +85,7 @@ echo "Compiling $executableName ..."
 if	[ $wantInstall -eq 1 ]
 then
 	exitCode=1
-	$GOROOT/bin/go install $compileFlags $projectDir && exitCode=0
+	$GOROOT/bin/go install $compileFlags && exitCode=0
 	if [ $exitCode  -eq 0 ]
 	then
 		placedPath="$GOPATH/bin"
@@ -87,7 +94,7 @@ then
 	fi
 else
 	exitCode=1
-	$GOROOT/bin/go build $compileFlags $projectDir  && exitCode=0
+	$GOROOT/bin/go build $compileFlags  && exitCode=0
 	if [ $exitCode -eq 0 ]
 	then
 		placedPath=$("pwd")
@@ -96,14 +103,19 @@ else
 	fi
 fi
 
-$GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/fsreplacer/fsreplacer $PKG_CONFIG_PATH/assimp.pc $GOPATH '%GOPATH%'
+if [ $wantRelease -eq 1 ]
+then
+	strip -s $placedPath/$executableName.exe
+fi
+
+$GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/fsreplacer/fsreplacer $PKG_CONFIG_PATH/assimp.pc $($GOPATH/src/github.com/PucklaMotzer09/gohomeengine/cmd/bsreplacer/bsreplacer.exe $GOPATH) '%GOPATH%'
 
 echo Placed executable in $placedPath
 
 if [ $wantRun -eq 1 ]
 then
 	exitCode=1
-	$placedPath/$executableName && exitCode=0
+	$placedPath/$executableName.exe && exitCode=0
 	exit $exitCode
 fi
 
