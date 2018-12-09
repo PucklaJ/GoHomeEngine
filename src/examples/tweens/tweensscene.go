04f4a14332908e1f1160a42433b981f33f8c0fed
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-12-09 22:12:38 +0100

    Added README.md to tweens example

--- src/examples/tweens/tweensscene.go
+++ src/examples/tweens/tweensscene.go
@@ -13,24 +13,24 @@ type TweensScene struct {
 func (this *TweensScene) Init() {
 	gohome.Init2DShaders()
 
-	gohome.ResourceMgr.LoadTexture("Gopher","gopher.png")
+	gohome.ResourceMgr.LoadTexture("Gopher", "gopher.png")
 
 	this.gopher.Init("Gopher")
-	this.gopher.Transform.Origin = [2]float32{0.5,0.5}
-	nw,nh := gohome.Render.GetNativeResolution()
-	this.gopher.Transform.Position = [2]float32{0.0,0.0}
-	this.gopher.Transform.Scale = [2]float32{0.8,0.8}
+	this.gopher.Transform.Origin = [2]float32{0.5, 0.5}
+	nr := gohome.Render.GetNativeResolution()
+	this.gopher.Transform.Position = [2]float32{0.0, 0.0}
+	this.gopher.Transform.Scale = [2]float32{0.8, 0.8}
 	this.tset = gohome.Tweenset{
 		Tweens: []gohome.Tween{
-			&gohome.TweenBlink{Amount:80,Time:20.0,TweenType:gohome.TWEEN_TYPE_ALWAYS},
-			&gohome.TweenPosition2D{Destination:[2]float32{float32(nw),0.0},Time:5.0,TweenType:gohome.TWEEN_TYPE_AFTER_PREVIOUS},
-			&gohome.TweenScale2D{Destination:[2]float32{0.8*2.0,0.8*2.0},Time:5.0,TweenType:gohome.TWEEN_TYPE_WITH_PREVIOUS},
-			&gohome.TweenPosition2D{Destination:[2]float32{float32(nw),float32(nh)},Time:5.0,TweenType:gohome.TWEEN_TYPE_AFTER_PREVIOUS,},
-			&gohome.TweenRotation2D{Destination:180.0,Time:5.0,TweenType:gohome.TWEEN_TYPE_WITH_PREVIOUS},
-			&gohome.TweenPosition2D{Destination:[2]float32{0.0,float32(nh)},Time:5.0,TweenType:gohome.TWEEN_TYPE_AFTER_PREVIOUS,},
-			&gohome.TweenPosition2D{Destination:[2]float32{0.0,0.0},Time:5.0,TweenType:gohome.TWEEN_TYPE_AFTER_PREVIOUS,},
-			&gohome.TweenScale2D{Destination:[2]float32{0.8,0.8},Time:5.0,TweenType:gohome.TWEEN_TYPE_WITH_PREVIOUS},
-			&gohome.TweenRotation2D{Destination:360.0,Time:5.0,TweenType:gohome.TWEEN_TYPE_WITH_PREVIOUS},
+			&gohome.TweenBlink{Amount: 80, Time: 20.0, TweenType: gohome.TWEEN_TYPE_ALWAYS},
+			&gohome.TweenPosition2D{Destination: [2]float32{nr[0], 0.0}, Time: 5.0, TweenType: gohome.TWEEN_TYPE_AFTER_PREVIOUS},
+			&gohome.TweenScale2D{Destination: [2]float32{0.8 * 2.0, 0.8 * 2.0}, Time: 5.0, TweenType: gohome.TWEEN_TYPE_WITH_PREVIOUS},
+			&gohome.TweenPosition2D{Destination: nr, Time: 5.0, TweenType: gohome.TWEEN_TYPE_AFTER_PREVIOUS},
+			&gohome.TweenRotation2D{Destination: 180.0, Time: 5.0, TweenType: gohome.TWEEN_TYPE_WITH_PREVIOUS},
+			&gohome.TweenPosition2D{Destination: [2]float32{0.0, nr[1]}, Time: 5.0, TweenType: gohome.TWEEN_TYPE_AFTER_PREVIOUS},
+			&gohome.TweenPosition2D{Destination: [2]float32{0.0, 0.0}, Time: 5.0, TweenType: gohome.TWEEN_TYPE_AFTER_PREVIOUS},
+			&gohome.TweenScale2D{Destination: [2]float32{0.8, 0.8}, Time: 5.0, TweenType: gohome.TWEEN_TYPE_WITH_PREVIOUS},
+			&gohome.TweenRotation2D{Destination: 360.0, Time: 5.0, TweenType: gohome.TWEEN_TYPE_WITH_PREVIOUS},
 		},
 		Loop: true,
 	}
@@ -40,8 +40,8 @@ func (this *TweensScene) Init() {
 	gohome.RenderMgr.AddObject(&this.gopher)
 
 	this.cam.Zoom = 0.5
-	this.cam.Position = [2]float32{-float32(nw)*0.5,-float32(nh)*0.5}
-	gohome.RenderMgr.SetCamera2D(&this.cam,0)
+	this.cam.Position = nr.Div(-2.0)
+	gohome.RenderMgr.SetCamera2D(&this.cam, 0)
 }
 
 func (this *TweensScene) Update(delta_time float32) {
