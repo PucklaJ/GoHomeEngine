7d1d8c174843a97678dc84f7eff004c24f128c80
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-01-12 15:15:12 +0100

    Updated gitignore and changed some names of files

---
+++ src/examples/audio/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/PucklaMotzer09/GoHomeEngine/src/frameworks/SDL2"
+	"github.com/PucklaMotzer09/GoHomeEngine/src/gohome"
+	"github.com/PucklaMotzer09/GoHomeEngine/src/renderers/OpenGLES2"
+)
+
+func main() {
+	gohome.MainLop.Run(&framework.SDL2Framework{},&renderer.OpenGLES2Renderer{},640,480,"Audio",&AudioScene{})
+}
---
+++ src/examples/cube/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/PucklaMotzer09/GoHomeEngine/src/frameworks/GLFW"
+	"github.com/PucklaMotzer09/GoHomeEngine/src/gohome"
+	"github.com/PucklaMotzer09/GoHomeEngine/src/renderers/OpenGL"
+)
+
+func main() {
+	gohome.MainLop.Run(&framework.GLFWFramework{}, &renderer.OpenGLRenderer{}, 640, 480, "Cube", &CubeScene{})
+}
