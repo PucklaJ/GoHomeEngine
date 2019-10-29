afd370c96f6f95b45b2eafffbbb52f0686156cc7
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-29 14:30:29 +0100

    Add comments to cubemap.go

--- src/gohome/cubemap.go
+++ src/gohome/cubemap.go
@@ -5,26 +5,45 @@ import (
 	"image/color"
 )
 
+// This interface represents a cube map with six faces
 type CubeMap interface {
+	// Loads a cube map from data with width and height
 	Load(data []byte, width, height int, shadowMap bool)
+	// Loads the cube map from an image
 	LoadFromImage(img image.Image)
+	// Binds the cube map to unit
 	Bind(unit uint32)
+	// Unbinds cube map
 	Unbind(unit uint32)
+	// Returns the width of the cube map
 	GetWidth() int
+	// Returns the height of the cube map
 	GetHeight() int
+	// Returns the color that will be keyed
 	GetKeyColor() color.Color
+	// Returns the modulate color
 	GetModColor() color.Color
+	// Cleans everything up
 	Terminate()
+	// Sets filtering to the give method
 	SetFiltering(filtering int)
+	// Sets wrapping to the given method
 	SetWrapping(wrapping int)
+	// Sets the color of the border to col if used
 	SetBorderColor(col color.Color)
+	// Sets the depth of the border
 	SetBorderDepth(depth float32)
+	// Sets the key color
 	SetKeyColor(col color.Color)
+	// Sets the modulate color
 	SetModColor(col color.Color)
+	// Returns the name of this cube map
 	GetName() string
+	// Returns the data its with and its height
 	GetData() ([]byte, int, int)
 }
 
+// An implementation of CubeMap that does nothing
 type NilCubeMap struct {
 }
 
