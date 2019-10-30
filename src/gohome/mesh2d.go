ff5842fed34a3381775e9acfb192a9cbffa70218
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-30 10:08:29 +0100

    Add comments to mesh2d.go

--- src/gohome/mesh2d.go
+++ src/gohome/mesh2d.go
@@ -1,12 +1,18 @@
 package gohome
 
+// A 2D mesh consisting of geometry used for rendering 2D
 type Mesh2D interface {
+	// Add vertices and indices to the mesh
 	AddVertices(vertices []Mesh2DVertex, indices []uint32)
+	// Loads vertices and indices to the GPU
 	Load()
+	// Call the draw method on the data
 	Render()
+	// Cleans everything up
 	Terminate()
 }
 
+// An implementation of Mesh2D that does nothing
 type NilMesh2D struct {
 }
 
