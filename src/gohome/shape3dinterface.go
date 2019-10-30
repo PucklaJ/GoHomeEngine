f47ba5a3b7a6e0795da53d05448bb34f63532fcb
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-30 15:21:32 +0100

    Add comments to shape3dinterface.go

--- src/gohome/shape3dinterface.go
+++ src/gohome/shape3dinterface.go
@@ -1,17 +1,28 @@
 package gohome
 
+// An interface that handels low level stuff concerning 3D shapes
 type Shape3DInterface interface {
+	// Initialises everything
 	Init()
+	// Adds points to the shape
 	AddPoints(points []Shape3DVertex)
+	// Returns all points
 	GetPoints() []Shape3DVertex
+	// Set the draw mode (POINTS,LINES,TRIANGLES)
 	SetDrawMode(drawMode uint8)
+	// Sets the point size
 	SetPointSize(size float32)
+	// Sets the line width
 	SetLineWidth(size float32)
+	// Loads all data to the GPU
 	Load()
+	// Calls the draw method for the data
 	Render()
+	// Cleans everything up
 	Terminate()
 }
 
+// An implementation of Shape3DInterface that does nothing
 type NilShape3DInterface struct {
 }
 
