0c107bb48705510359e1785643a621a40d5618ee
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-30 08:33:37 +0100

    Add comments to instancedmesh3d.go

--- src/gohome/instancedmesh3d.go
+++ src/gohome/instancedmesh3d.go
@@ -4,6 +4,7 @@ import (
 	"github.com/PucklaMotzer09/mathgl/mgl32"
 )
 
+// The different types of values that can be used for instancing
 const (
 	VALUE_FLOAT = iota
 	VALUE_VEC2  = iota
@@ -14,39 +15,72 @@ const (
 	VALUE_MAT4  = iota
 )
 
+// A mesh that will be drawn using instancing
 type InstancedMesh3D interface {
+	// Adds vertices and indices to the mesh
 	AddVertices(vertices []Mesh3DVertex, indices []uint32)
+	// Loads the vertices and indices to the GPU
 	Load()
+	// Calls the draw method for this mesh
 	Render()
+	// Cleans everything up
 	Terminate()
+	// Sets the material of this mesh
 	SetMaterial(mat *Material)
+	// Returns the material of this mesh
 	GetMaterial() *Material
+	// Returns the name of this mesh
 	GetName() string
+	// Returns the number of vertices of this mesh
 	GetNumVertices() int
+	// Returns the number of indices of this mesh
 	GetNumIndices() int
+	// Returns all the vertices of this mesh
 	GetVertices() []Mesh3DVertex
+	// Returns all the indices of this mesh
 	GetIndices() []uint32
+	// Calculates the tangents that will be used for a normal map
 	CalculateTangents()
+	// Returns wether the vertices have UV values
+	// Checks if the uv values are zero
 	HasUV() bool
+	// Returns the bounding box from the lowest vertex to the highest vertex
 	AABB() AxisAlignedBoundingBox
+	// Creates a new mesh from this mesh
 	Copy() Mesh3D
+	// Returns wether Load has been called
 	LoadedToGPU() bool
+	// Sets the number of buffered instances
 	SetNumInstances(n int)
+	// Returns the number of buffered instances
 	GetNumInstances() int
+	// Sets number of instances that will be drawn
 	SetNumUsedInstances(n int)
+	// Returns the number of instances that will be drawn
 	GetNumUsedInstances() int
+	// Adds a value that will be used for instancing
 	AddValue(valueType int)
+	// Adds a value that will be used for instancing to the front
 	AddValueFront(valueType int)
+	// Sets the float values of index, value needs to be of size num instances
 	SetF(index int, value []float32)
+	// Sets the Vec2 values of index, value needs to be of size num instances
 	SetV2(index int, value []mgl32.Vec2)
+	// Sets the vec3 values of index, value needs to be of size num instances	
 	SetV3(index int, value []mgl32.Vec3)
+	// Sets the vec4 values of index, value needs to be of size num instances	
 	SetV4(index int, value []mgl32.Vec4)
+	// Sets the Mat2 values of index, value needs to be of size num instances	
 	SetM2(index int, value []mgl32.Mat2)
+	// Sets the Mat3 values of index, value needs to be of size num instances	
 	SetM3(index int, value []mgl32.Mat3)
+	// Sets the Mat4 values of index, value needs to be of size num instances	
 	SetM4(index int, value []mgl32.Mat4)
+	// Sets the name of a value	
 	SetName(index int, value_type int, value string)
 }
 
+// Creates a instanced mesh from a Mesh3D
 func InstancedMesh3DFromMesh3D(mesh Mesh3D) (imesh InstancedMesh3D) {
 	if !mesh.LoadedToGPU() {
 		imesh = Render.CreateInstancedMesh3D(mesh.GetName())
@@ -62,6 +96,7 @@ func InstancedMesh3DFromMesh3D(mesh Mesh3D) (imesh InstancedMesh3D) {
 	return
 }
 
+// An implementation of InstancedMesh3D that does nothing
 type NilInstancedMesh3D struct {
 }
 
