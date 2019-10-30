8d146c14e8942bf10c04aac1734900d56b3976aa
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-30 12:39:49 +0100

    Add comments to scenemanager.go

--- src/gohome/scenemanager.go
+++ src/gohome/scenemanager.go
@@ -1,11 +1,16 @@
 package gohome
 
+// A scene that can be switched
 type Scene interface {
+	// Init is called when you switch scenes
 	Init()
+	// Update is called every frame
 	Update(delta_time float32)
+	// Terminate is called on the current scene when the scene is switched
 	Terminate()
 }
 
+// An implementation of Scene that does nothing
 type NilScene struct {
 }
 
@@ -21,6 +26,7 @@ func (this *NilScene) Terminate() {
 
 }
 
+// The manager that handles the scene switching
 type SceneManager struct {
 	currentScene Scene
 }
@@ -29,6 +35,7 @@ func (scmgr *SceneManager) Init() {
 
 }
 
+// Switch to another scene
 func (scmgr *SceneManager) SwitchScene(scn Scene) {
 	if scmgr.currentScene != nil {
 		UpdateMgr.RemoveObject(scmgr.currentScene)
@@ -44,14 +51,17 @@ func (scmgr *SceneManager) SwitchScene(scn Scene) {
 	UpdateMgr.BreakUpdateLoop()
 }
 
+// Returns the current scene
 func (scmgr *SceneManager) GetCurrentScene() Scene {
 	return scmgr.currentScene
 }
 
+// Calls Terminate on the current scene
 func (scmgr *SceneManager) Terminate() {
 	if scmgr.currentScene != nil {
 		scmgr.currentScene.Terminate()
 	}
 }
 
+// The SceneManager that should be used for everything
 var SceneMgr SceneManager
