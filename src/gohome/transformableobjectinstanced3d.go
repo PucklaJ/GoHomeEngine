c00703f6b8a9334d5406f220929ff5cb8ebec0a0
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-31 08:41:49 +0100

    Add comments to transformableobjectinstanced3d.go

--- src/gohome/transformableobjectinstanced3d.go
+++ src/gohome/transformableobjectinstanced3d.go
@@ -4,21 +4,25 @@ import (
 	"github.com/PucklaMotzer09/mathgl/mgl32"
 )
 
+// A transformable object used for instancing
 type TransformableObjectInstanced3D struct {
 	TransformableObject3D
 
 	finalTransformMatrix *mgl32.Mat4
 }
 
+// Calculates the transform matrix
 func (tobj *TransformableObjectInstanced3D) CalculateTransformMatrix(rmgr *RenderManager, notRelativeToCamera int) {
 	tobj.TransformableObject3D.CalculateTransformMatrix(rmgr, notRelativeToCamera)
 	*tobj.finalTransformMatrix = tobj.camNotRelativeMatrix
 }
 
+// Returns the transform matrix that represents this transformation
 func (tobj *TransformableObjectInstanced3D) GetTransformMatrix() mgl32.Mat4 {
 	return *tobj.finalTransformMatrix
 }
 
+// Sets a pointer to a matrix so that it can be used for instancing
 func (tobj *TransformableObjectInstanced3D) SetTransformMatrixPointer(tmp *mgl32.Mat4) {
 	tobj.finalTransformMatrix = tmp
 }
