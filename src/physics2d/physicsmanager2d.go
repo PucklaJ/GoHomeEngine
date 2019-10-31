4b91f7576d2df2406297b0955313bb4c6a19b198
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-31 09:25:39 +0100

    Add comments to physicsmanager2d.go

--- src/physics2d/physicsmanager2d.go
+++ src/physics2d/physicsmanager2d.go
@@ -12,20 +12,27 @@ import (
 )
 
 var (
+	// Defines how big one meter is
 	PIXEL_PER_METER     float32 = 100.0
+	// Defines the size of the world (can be used to remove everything outside of the world)
 	WORLD_SIZE          mgl32.Vec2
+	// The number of velocity iterations of Box2D
 	VELOCITY_ITERATIONS = 6
+	// The number of position iterations of Box2D
 	POSITION_ITERATIONS = 2
 )
 
+// Converts a physics scalar value to a pixel value
 func ScalarToPixel(v float64) float32 {
 	return float32(v) * PIXEL_PER_METER
 }
 
+// Converts a pixel scalar value to a physics value
 func ScalarToBox2D(v float32) float64 {
 	return float64(v / PIXEL_PER_METER)
 }
 
+// Converts a pixel Vec2 to a physics Vec2
 func Vec2ToB2Vec2(vec mgl32.Vec2) box2d.B2Vec2 {
 	return box2d.B2Vec2{
 		ScalarToBox2D(vec[0]),
@@ -33,6 +40,7 @@ func Vec2ToB2Vec2(vec mgl32.Vec2) box2d.B2Vec2 {
 	}
 }
 
+// Converts a physics Vec2 to a pixel Vec2
 func B2Vec2ToVec2(vec box2d.B2Vec2) mgl32.Vec2 {
 	return mgl32.Vec2{
 		ScalarToPixel(vec.X),
@@ -40,43 +48,53 @@ func B2Vec2ToVec2(vec box2d.B2Vec2) mgl32.Vec2 {
 	}
 }
 
+// Converts a physics direction into a pixel direction
 func ToPixelDirection(vec box2d.B2Vec2) mgl32.Vec2 {
 	vec1 := B2Vec2ToVec2(vec)
 	vec1[1] = -vec1[1]
 	return vec1
 }
 
+// Converts a pixel direction into a physics direction
 func ToBox2DDirection(vec mgl32.Vec2) box2d.B2Vec2 {
 	vec[1] = -vec[1]
 	return Vec2ToB2Vec2(vec)
 }
 
+// Converts physics coordinates to pixel coordinates
 func ToPixelCoordinates(vec box2d.B2Vec2) mgl32.Vec2 {
 	vec1 := B2Vec2ToVec2(vec)
 	vec1[1] = WORLD_SIZE[1] - vec1[1]
 	return vec1
 }
 
+// Converts pixel coordinates to physics coordinates
 func ToBox2DCoordinates(vec mgl32.Vec2) box2d.B2Vec2 {
 	vec[1] = WORLD_SIZE[1] - vec[1]
 	return Vec2ToB2Vec2(vec)
 }
 
+// Converts a pixel angel to physics angle
 func ToBox2DAngle(angle float32) float64 {
 	return float64(mgl32.DegToRad(angle))
 }
 
+// Converts a physics angle to pixel angle
 func ToPixelAngle(angle float64) float32 {
 	return mgl32.RadToDeg(float32(angle))
 }
 
+// The manager that handles all 2D physics
 type PhysicsManager2D struct {
+	// The Box2D world
 	World  box2d.B2World
+	// Wether the world is paused (no movement)
 	Paused bool
 
 	connectors []*PhysicsConnector2D
 }
 
+// Initialises the world and everything using a gravity value
 func (this *PhysicsManager2D) Init(gravity mgl32.Vec2) {
 	this.World = box2d.MakeB2World(ToBox2DDirection(gravity))
 	WORLD_SIZE = gohome.Render.GetNativeResolution()
@@ -85,6 +103,7 @@ func (this *PhysicsManager2D) Init(gravity mgl32.Vec2) {
 	gohome.ErrorMgr.Log("Physics", "Box2D", "Initialized!")
 }
 
+// Gets called every frame and updates the physics
 func (this *PhysicsManager2D) Update(delta_time float32) {
 	if this.Paused {
 		return
@@ -110,6 +129,7 @@ func (this *PhysicsManager2D) Update(delta_time float32) {
 	}
 }
 
+// Creates a box that can move around
 func (this *PhysicsManager2D) CreateDynamicBox(pos mgl32.Vec2, size mgl32.Vec2) *box2d.B2Body {
 	bodyDef := box2d.MakeB2BodyDef()
 	bodyDef.Type = box2d.B2BodyType.B2_dynamicBody
@@ -121,6 +141,7 @@ func (this *PhysicsManager2D) CreateDynamicBox(pos mgl32.Vec2, size mgl32.Vec2) 
 	return body
 }
 
+// Creates a box that sticks to a position
 func (this *PhysicsManager2D) CreateStaticBox(pos mgl32.Vec2, size mgl32.Vec2) *box2d.B2Body {
 	bodyDef := box2d.MakeB2BodyDef()
 	bodyDef.Type = box2d.B2BodyType.B2_staticBody
@@ -137,6 +158,7 @@ func (this *PhysicsManager2D) CreateStaticBox(pos mgl32.Vec2, size mgl32.Vec2) *
 	return body
 }
 
+// Creates a circle that can move around
 func (this *PhysicsManager2D) CreateDynamicCircle(pos mgl32.Vec2, radius float32) *box2d.B2Body {
 	bodyDef := box2d.MakeB2BodyDef()
 	bodyDef.Type = box2d.B2BodyType.B2_dynamicBody
@@ -148,6 +170,7 @@ func (this *PhysicsManager2D) CreateDynamicCircle(pos mgl32.Vec2, radius float32
 	return body
 }
 
+// Creates a circle that sticks to a position
 func (this *PhysicsManager2D) CreateStaticCircle(pos mgl32.Vec2, radius float32) *box2d.B2Body {
 	bodyDef := box2d.MakeB2BodyDef()
 	bodyDef.Type = box2d.B2BodyType.B2_staticBody
@@ -164,6 +187,7 @@ func (this *PhysicsManager2D) CreateStaticCircle(pos mgl32.Vec2, radius float32)
 	return body
 }
 
+// Returns a new debug drawer
 func (this *PhysicsManager2D) GetDebugDraw() PhysicsDebugDraw2D {
 	return PhysicsDebugDraw2D{
 		mgr:        this,
@@ -174,6 +198,7 @@ func (this *PhysicsManager2D) GetDebugDraw() PhysicsDebugDraw2D {
 	}
 }
 
+// Converts the objects of a tmx map layer to static collision objects
 func (this *PhysicsManager2D) LayerToCollision(tiledmap *gohome.TiledMap, layerName string) (bodies []*box2d.B2Body) {
 	layers := tiledmap.Layers
 	for i := 0; i < len(layers); i++ {
@@ -212,6 +237,7 @@ func (this *PhysicsManager2D) LayerToCollision(tiledmap *gohome.TiledMap, layerN
 	return
 }
 
+// Creates an ellipse
 func (this *PhysicsManager2D) CreateEllipse(X, Y, Width, Height float64) *box2d.B2Body {
 	radius := float32((Width + Height) / 2.0 / 2.0)
 	pos := [2]float32{float32(X) + radius, float32(Y) + radius}
@@ -219,6 +245,7 @@ func (this *PhysicsManager2D) CreateEllipse(X, Y, Width, Height float64) *box2d.
 	return this.CreateStaticCircle(pos, radius)
 }
 
+// Creates a rectangle
 func (this *PhysicsManager2D) CreateRectangle(X, Y, Width, Height float64) *box2d.B2Body {
 	size := mgl32.Vec2{float32(Width), float32(Height)}
 	pos := mgl32.Vec2{float32(X), float32(Y)}.Add(size.Mul(0.5))
@@ -226,6 +253,7 @@ func (this *PhysicsManager2D) CreateRectangle(X, Y, Width, Height float64) *box2
 	return this.CreateStaticBox(pos, size)
 }
 
+// Creates a polygon from a tmx polygon
 func (this *PhysicsManager2D) CreatePolygon(X, Y float64, poly *tmx.Polygon) *box2d.B2Body {
 	points := strings.Split(poly.Points, " ")
 	if len(points) > 8 {
@@ -260,6 +288,7 @@ func (this *PhysicsManager2D) CreatePolygon(X, Y float64, poly *tmx.Polygon) *bo
 	return body
 }
 
+// Creates a polyline from a tmx polyline
 func (this *PhysicsManager2D) CreatePolyline(X, Y float64, line *tmx.Polyline) *box2d.B2Body {
 	bodyDef := box2d.MakeB2BodyDef()
 	bodyDef.Type = box2d.B2BodyType.B2_staticBody
@@ -290,6 +319,7 @@ func (this *PhysicsManager2D) CreatePolyline(X, Y float64, line *tmx.Polyline) *
 	return body
 }
 
+// Destroys all bodies and the world
 func (this *PhysicsManager2D) Terminate() {
 	for b := this.World.GetBodyList(); b != nil; b = b.GetNext() {
 		this.World.DestroyBody(b)
